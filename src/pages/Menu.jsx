e6a30666583f3d84e2745226e608c8a1f9e43676
Parvej101 <you@example.com>
2025-01-05 22:50:49 +0600

    added data by cathegory

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,13 +1,43 @@
 import { Helmet } from "react-helmet-async";
+import dessertImg from '../../src/assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../src/assets/menu/pizza-bg.jpg';
+import saladImg from '../../src/assets/menu/salad-bg.jpg';
+import soupImg from '../../src/assets/menu/soup-bg.jpg';
+
+import useMenu from "../assets/hook/menuHook";
+import SectionTitle from "../components/SectionTitle/SectionTitle";
+import MenuCategory from "./Shared/MenuCategory/MenuCategory";
 
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const salad = menu.filter(item => item.category === 'salad');
+    const soup = menu.filter(item => item.category === 'soup');
+    const offered = menu.filter(item => item.category === 'offered');
+    const pizza = menu.filter(item => item.category === 'pizza');
+
     return (
         <div>
-            <Helmet>
-        <title>Bistro Boss | Menu</title>
-      </Helmet>
-            This is menu    
+            <div>
+                <Helmet>
+                    <title>Bistro Boss | Menu</title>
+                </Helmet>
+                
+            </div>
+            {/* main cover */}
+            <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle>
+            {/* offerd */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert */}
+            <MenuCategory 
+            items={pizza}
+            title={'Pizza'}
+            coverImg={pizzaImg}
+             ></MenuCategory>
+            <MenuCategory items={dessert} title={'Dessert'} coverImg={dessertImg}></MenuCategory>
+            <MenuCategory items={salad} title={'Salad'} coverImg={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={'Soup'} coverImg={soupImg}></MenuCategory>
         </div>
     );
 };
